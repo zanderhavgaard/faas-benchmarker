58669d6b35668e7f5759919d70a78567fb80ea2a
zanderhavgaard <zander@havgaard.dk>
2020-03-05 00:29:32 +0100

    added api token authentication to lambda terraform example

--- infrastructure/aws_lambda_template/aws_lambda_template.tf
+++ infrastructure/aws_lambda_template/aws_lambda_template.tf
@@ -77,35 +77,42 @@ resource "aws_api_gateway_method" "hw-lambda-method" {
   resource_id = aws_api_gateway_resource.hw-api-resource.id
   http_method = "ANY"
   authorization = "None" # TODO add auth
+  api_key_required = true
 }
 
 # point API endpoint at lambda function
 resource "aws_api_gateway_integration" "hw-api-integration" {
-   rest_api_id = aws_api_gateway_rest_api.hw-api.id
-   resource_id = aws_api_gateway_method.hw-lambda-method.resource_id
-   http_method = aws_api_gateway_method.hw-lambda-method.http_method
-   integration_http_method = "POST"
-   type                    = "AWS_PROXY"
-   uri                     = aws_lambda_function.hw-python.invoke_arn
- }
+  rest_api_id = aws_api_gateway_rest_api.hw-api.id
+  resource_id = aws_api_gateway_method.hw-lambda-method.resource_id
+  http_method = aws_api_gateway_method.hw-lambda-method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.hw-python.invoke_arn
+}
 
 
- # root
- resource "aws_api_gateway_method" "proxy_root" {
-   rest_api_id   = aws_api_gateway_rest_api.hw-api.id
-   resource_id   = aws_api_gateway_rest_api.hw-api.root_resource_id
-   http_method   = "ANY"
-   authorization = "NONE"
- }
- resource "aws_api_gateway_integration" "lambda_root" {
-   rest_api_id = aws_api_gateway_rest_api.hw-api.id
-   resource_id = aws_api_gateway_method.proxy_root.resource_id
-   http_method = aws_api_gateway_method.proxy_root.http_method
-   integration_http_method = "POST"
-   type                    = "AWS_PROXY"
-   uri                     = aws_lambda_function.hw-python.invoke_arn
- }
- # end root
+
+
+# root
+resource "aws_api_gateway_method" "proxy_root" {
+  rest_api_id   = aws_api_gateway_rest_api.hw-api.id
+  resource_id   = aws_api_gateway_rest_api.hw-api.root_resource_id
+  http_method   = "ANY"
+  authorization = "NONE"
+  api_key_required = true
+}
+resource "aws_api_gateway_integration" "lambda_root" {
+  rest_api_id = aws_api_gateway_rest_api.hw-api.id
+  resource_id = aws_api_gateway_method.proxy_root.resource_id
+  http_method = aws_api_gateway_method.proxy_root.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.hw-python.invoke_arn
+}
+# end root
+
+
+
 
 
 # add a deployment of the api
@@ -130,7 +137,45 @@ resource "aws_lambda_permission" "apigw" {
   source_arn = "${aws_api_gateway_rest_api.hw-api.execution_arn}/*/*"
 }
 
+# create api useage plan key
+resource "aws_api_gateway_api_key" "hw-key" {
+  name = "hw-key"
+}
+
+# create api usage plan
+resource "aws_api_gateway_usage_plan" "hw" {
+  name         = "hw-usage-plan"
+
+  api_stages {
+    api_id = aws_api_gateway_rest_api.hw-api.id
+    stage  = aws_api_gateway_deployment.hw-prod.stage_name
+  }
+
+  quota_settings {
+    limit  = 2000
+    offset = 0
+    period = "DAY"
+  }
+
+  throttle_settings {
+    burst_limit = 1000
+    rate_limit  = 1000
+  }
+}
+
+# attach api key to useage plan
+resource "aws_api_gateway_usage_plan_key" "hw" {
+  key_id        = aws_api_gateway_api_key.hw-key.id
+  key_type      = "API_KEY"
+  usage_plan_id = aws_api_gateway_usage_plan.hw.id
+}
+
 # output the invocation url
 output "invoke_url" {
   value = aws_api_gateway_deployment.hw-prod.invoke_url
+}
+
+# output the api key
+output "api_key" {
+  value = aws_api_gateway_api_key.hw-key.value
 }
