68255db2424ecb83ec9f7814ce9817a59227992f
zanderhavgaard <zander@havgaard.dk>
2020-03-05 16:05:18 +0100

    added second lambda to aws lambda template example

--- infrastructure/aws_lambda_template/aws_lambda_template.tf
+++ infrastructure/aws_lambda_template/aws_lambda_template.tf
@@ -27,6 +27,13 @@ data "archive_file" "hw-lambda-code" {
   output_path = "lambda.zip"
 }
 
+# creates zip archive containing lambda code
+data "archive_file" "hw2-lambda-code" {
+  type = "zip"
+  source_file = "lambda_function2.py"
+  output_path = "lambda2.zip"
+}
+
 # create lambda function
 resource "aws_lambda_function" "hw-python" {
   filename = data.archive_file.hw-lambda-code.output_path
@@ -35,6 +42,16 @@ resource "aws_lambda_function" "hw-python" {
   handler = "lambda_function.lambda_handler"
   runtime = "python3.7"
   source_code_hash = filemd5(data.archive_file.hw-lambda-code.output_path)
+  publish = true
+}
+
+resource "aws_lambda_function" "hw2-python" {
+  filename = data.archive_file.hw2-lambda-code.output_path
+  function_name = "hw2-python"
+  role = aws_iam_role.hw-role.arn
+  handler = "lambda_function2.lambda_handler"
+  runtime = "python3.7"
+  source_code_hash = filemd5(data.archive_file.hw2-lambda-code.output_path)
   publish = true
 }
 
@@ -90,15 +107,6 @@ resource "aws_api_gateway_integration" "hw-api-integration" {
   uri                     = aws_lambda_function.hw-python.invoke_arn
 }
 
-# add a deployment of the api
-resource "aws_api_gateway_deployment" "hw-prod" {
-  depends_on = [
-    aws_api_gateway_integration.hw-api-integration,
-  ]
-  rest_api_id = aws_api_gateway_rest_api.hw-api.id
-  stage_name = "prod"
-}
-
 # add permission for gateway to invoke lambdas
 resource "aws_lambda_permission" "apigw" {
   statement_id  = "AllowAPIGatewayInvoke"
@@ -109,6 +117,54 @@ resource "aws_lambda_permission" "apigw" {
   # The "/*/*" portion grants access from any method on any resource
   # within the API Gateway REST API.
   source_arn = "${aws_api_gateway_rest_api.hw-api.execution_arn}/*/*"
+}
+
+# create API endpoint
+resource "aws_api_gateway_resource" "hw2-api-resource" {
+  rest_api_id = aws_api_gateway_rest_api.hw-api.id
+  parent_id = aws_api_gateway_rest_api.hw-api.root_resource_id
+  path_part = "hello2"
+}
+
+# create API endpoint method
+resource "aws_api_gateway_method" "hw2-lambda-method" {
+  rest_api_id = aws_api_gateway_rest_api.hw-api.id
+  resource_id = aws_api_gateway_resource.hw2-api-resource.id
+  http_method = "GET"
+  authorization = "None" # TODO add auth
+  api_key_required = true
+}
+
+# point API endpoint at lambda function
+resource "aws_api_gateway_integration" "hw2-api-integration" {
+  rest_api_id = aws_api_gateway_rest_api.hw-api.id
+  resource_id = aws_api_gateway_method.hw2-lambda-method.resource_id
+  http_method = aws_api_gateway_method.hw2-lambda-method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.hw2-python.invoke_arn
+}
+
+# add permission for gateway to invoke lambdas
+resource "aws_lambda_permission" "apigw2" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hw2-python.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The "/*/*" portion grants access from any method on any resource
+  # within the API Gateway REST API.
+  source_arn = "${aws_api_gateway_rest_api.hw-api.execution_arn}/*/*"
+}
+
+# add a deployment of the api
+resource "aws_api_gateway_deployment" "hw-prod" {
+  depends_on = [
+    aws_api_gateway_integration.hw-api-integration,
+    aws_api_gateway_integration.hw2-api-integration,
+  ]
+  rest_api_id = aws_api_gateway_rest_api.hw-api.id
+  stage_name = "prod"
 }
 
 # create api useage plan key
