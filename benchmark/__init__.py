84c6db664195180331cece4ecafb77f5aafe1f26
zanderhavgaard <zander@havgaard.dk>
2020-04-28 14:54:23 +0200

    added python package file to benchmark directory

---
+++ benchmark/__init__.py
@@ -0,0 +1,5 @@
+from .benchmarker import Benchmark
+from .provider_abstract import AbstractProvider
+from .provider_aws_lambda import AWSLambdaProvider
+from .provider_azure_functions import AzureFunctionsProvider
+from .provider_openfaas import OpenFaasProvider
