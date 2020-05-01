6586a22b9d1656902b6c26f057848ff35aee0b06
Thomas <thomas@localhost.localdomain>
2020-05-01 14:28:20 +0200

    refactored openfaas cloud functions with exceptions, levels, and parent

--- benchmark/provider_openfaas.py
+++ benchmark/provider_openfaas.py
@@ -5,16 +5,17 @@ import dotenv
 import os
 from provider_abstract import AbstractProvider
 import traceback
+from pprint import pprint
 
 
 class OpenFaasProvider(AbstractProvider):
 
     def __init__(self, env_file_path: str) -> None:
-       
+
         # load aws lambda specific invocation url and credentials
         self.load_env_vars(env_file_path)
         # added for testing with .test_env
-        self.gateway_url = os.getenv('invoke_url')
+        self.gateway_url = str(os.getenv('invoke_url'))
         # http headers, contains authentication and data type
         self.headers = {
             'Content-Type': 'application/json'
@@ -27,12 +28,13 @@ class OpenFaasProvider(AbstractProvider):
     def invoke_function(self,
                         function_endpoint: str,
                         sleep: float = 0.0,
-                        invoke_nested: dict = None
+                        invoke_nested: list = None
                         ) -> dict:
 
         # paramters, the only required paramter is the statuscode
         params = {
             "StatusCode": 200
+            # "level": 0
         }
 
         # add optional sleep parameter if present
@@ -49,7 +51,7 @@ class OpenFaasProvider(AbstractProvider):
 
         # create url of function to invoke
         invoke_url = f'{self.gateway_url}/{function_endpoint}'
-       
+
         try:
 
             # log start time of invocation
@@ -61,7 +63,7 @@ class OpenFaasProvider(AbstractProvider):
                 headers=self.headers,
                 data=json.dumps(params)
             )
-            
+
             # log the end time of the invocation
             end_time = time.time()
 
@@ -69,7 +71,7 @@ class OpenFaasProvider(AbstractProvider):
             #  print('provider')
             #  print(response)
             #  print(response.content.decode())
-          
+
             #  import sys
             #  sys.exit()
 
@@ -78,6 +80,8 @@ class OpenFaasProvider(AbstractProvider):
             if(response.status_code == 200):
 
                 response_json = json.loads(response.content.decode())
+                # pprint(response_json)
+                # print()
 
                 #  import sys
                 #  sys.exit()
@@ -87,6 +91,7 @@ class OpenFaasProvider(AbstractProvider):
 
                 # parse response body
                 response_data = json.loads(response_json['body'])
+                # pprint(response_data)
 
                 # add invocation metadata to response
                 response_data[identifier]['invocation_start'] = start_time
@@ -96,23 +101,23 @@ class OpenFaasProvider(AbstractProvider):
                 return response_data
 
             else:
-              
+
                 error_dict = {
-                            'Error': {
-                                'identifier': function_endpoint+'-None-'+str(response.status_code),
-                                'uuid': None,
-                                'sleep': sleep,
-                                'ip_address': None,
-                                'python_version': None,
-                                'hostname': None,
-                                'invocation_start': start_time,
-                                'invocation_end': end_time,
-                                'invocation_start': None,
-                                'invocation_end': None,
-                                'status_code': response.status_code
-                                },
-                                'root_identifier': function_endpoint+'-None-'+str(response.status_code),
-                             }
+                    'Error': {
+                        'identifier': function_endpoint+'-None-'+str(response.status_code),
+                        'uuid': None,
+                        'sleep': sleep,
+                        'ip_address': None,
+                        'python_version': None,
+                        'hostname': None,
+                        'invocation_start': start_time,
+                        'invocation_end': end_time,
+                        'invocation_start': None,
+                        'invocation_end': None,
+                        'status_code': response.status_code
+                    },
+                    'root_identifier': function_endpoint+'-None-'+str(response.status_code),
+                }
                 return error_dict
 
         except Exception as e:
@@ -126,5 +131,4 @@ class OpenFaasProvider(AbstractProvider):
             #                             }
             #                 }
             # return exception_dict
-            print('caught exception in openfaas of type',type(e), str(e), tb) 
-
+            print('caught exception in openfaas of type', type(e), str(e), tb)
