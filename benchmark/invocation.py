d6be2b888e3121bbd44668fe34fdcef822e9a9ef
Thomas <thomas@localhost.localdomain>
2020-05-11 15:26:49 +0200

    errorlog for invocations

--- benchmark/invocation.py
+++ benchmark/invocation.py
@@ -34,6 +34,7 @@ class Invocation:
             self.type = self.error['type']
             self.trace = self.error['trace'].replace('\'','*')+'all qoutation marks have been replaced with *'
             self.message = self.error['message']
+            self.write_errorlog(self.error,self.identifier)
             delattr(self,'error')
         else:
             self.is_error = False
@@ -61,5 +62,19 @@ class Invocation:
             
     def conver_unix_time(self,time:str):
         datetime.utcfromtimestamp(int(time)).strftime('%Y-%m-%d %H:%M:%S')
+    
+    def write_errorlog(self, error_dict:dict, id:str):
+
+        with open("/home/ubuntu/ErrorLogFile.log","a+") as f:
+        #  with open("/home/thomas/ErrorLogFile.log","a+") as f:
+
+            f.write('An error occurred in a cloud function invocation'+'\n')
+            f.write('function UUID: '+id+'\n')
+            f.write(str(datetime.now()) + '\n')
+            f.write('type: ' + error_dict['type']+'«\n')
+            f.write('message: '+error_dict['message']+'\n')
+            f.write('trace: '+error_dict['trace']+'\n')
+            f.write("--------------------------\n")
+            f.close()
 
     
