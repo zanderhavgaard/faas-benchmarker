a244b4b67fc44ee892ad6c02c61a1b2981a17a26
Thomas <thomas@localhost.localdomain>
2020-05-12 12:17:12 +0200

    changed ssh_query to only return dataframes and db:interface to accommodate, some new cpu_effeciency methods for db_interface

--- benchmark/concurrent-test.py
+++ benchmark/concurrent-test.py
@@ -5,6 +5,7 @@ from pprint import pprint
 from invocation import Invocation
 from ssh_query import SSH_query
 from mysql_interface import SQL_Interface
+from functools import reduce
 # import benchmark.provider_abstract as abstract
 
 
@@ -15,8 +16,8 @@ nested = [
        {
         "function_name": 'function1',
         "invoke_payload": {
-            "StatusCode": 500,
-            "sleep": "test"
+            "StatusCode": 200,
+            "throughput_time": 1.0
         }
     }
 ]
@@ -34,14 +35,17 @@ db_interface = SQL_Interface()
 
 # print('experiment',db_interface.get_most_recent_experiment(flag=False))
 # print()
-# print('invocations',db_interface.get_all_from_Invocation(flag=False))
+# print('invocations')
+# invocations = db_interface.get_most_recent_invocations(flag=False)
+# print('number of invocations',len(invocations))
+# pprint(invocations)
 # print()
 # print('errors',db_interface.get_all_from_Error(flag=False))
-print('all experiments',db_interface.get_all_experiments('id',flag=False))
-print()
-print()
+# print('all experiments',db_interface.get_all_experiments('id',flag=False))
+# print()
+# print()
 
-bench = bench('exp2','openfaas','foobar', 'testter','/home/thomas/Msc/faas-benchmarker/.test_env')
+bench = bench('exp3','openfaas','foobar', 'testter','/home/thomas/Msc/faas-benchmarker/.test_env')
 bench.invoke_function('function1',0.0,nested,0.3)
 bench.invoke_function('function1',0.0,nested,1.0)
 # bench.invoke_function_conccurrently('function2',throughput_time=0.8,numb_threads=8)
@@ -54,20 +58,46 @@ print('invocations',len(invocations))
 
 bench.log_experiment_running_time()
 
-print('experiment',db_interface.get_most_recent_experiment(flag=False))
-print()
-invos = db_interface.get_most_recent_invocations(args='function_cores',flag=False)
-print('length',len(invos[0]))
-print('invocations\n',invos)
+# print('experiment',db_interface.get_most_recent_experiment(flag=False))
+# print()
+# invos = db_interface.get_most_recent_invocations(flag=False)
+# print('length',len(invos[0]))
+# print('invocations\n',invos)
 
+# print()
+# print('errors',db_interface.get_most_recent_errors(flag=False))
+# # print('numbered',db_interface.get_explicit_number_experiments('uuid,cores',4, False,False))
+# # print('invocation',db_interface.get_explicit_number_invocations('uuid,exp_id',4,False,True))
+# print()
+# print('4 first errors',db_interface.get_explicit_number_errors('uuid,exp_id',4,False,False))
+# print()
+# print('4 last errors',db_interface.get_explicit_number_errors('uuid,exp_id',4,False,True))
+# print()
+# print('cpu effeciency')
+# cpu_efficiency = db_interface.cpu_efficiency_last_experiment()
+# print('acc',cpu_efficiency[0])
+# print('res list',cpu_efficiency[1])
+# print('input vals',cpu_efficiency[2])
+
+# # test = reduce(lambda x,y: x+y,cpu_efficiency[2])
+
+# for x in cpu_efficiency[2]:
+#     print(x[0])
+#     print(x[1])
+#     print(x[2])
+#     print()
+
+cpu_efficiencies = db_interface.cpu_efficiency_per_invocation()
+print('cpus',cpu_efficiencies)
+print('cpu efficiency for last experiment',db_interface.cpu_efficiency_last_experiment())
 print()
-print('errors',db_interface.get_most_recent_errors(flag=False))
-# print('numbered',db_interface.get_explicit_number_experiments('uuid,cores',4, False,False))
-# print('invocation',db_interface.get_explicit_number_invocations('uuid,exp_id',4,False,True))
-print()
-print('errors asc order',db_interface.get_explicit_number_errors('uuid,exp_id',4,False,False))
-print()
-print('errors desc order',db_interface.get_explicit_number_errors('uuid,exp_id',4,False,True))
+cpu_efficiencies_throughputfunction = db_interface.cpu_efficiency_for_throughput_per_invocation()
+print('cpu_throughput',cpu_efficiencies_throughputfunction)
+cpu_throughput_uuid = db_interface.cpu_efficiency_for_throughput_per_invocation(bench.experiment.uuid)
+print('with uuid',cpu_throughput_uuid)
+print('acc',db_interface.cpu_efficiency_for_throughput_experiment())
+print('acc with uuid',db_interface.cpu_efficiency_for_throughput_experiment(bench.experiment.uuid))
+
 
 # print('invocations',db_interface.get_all_from_Invocation(flag=False))
 # print()
--- benchmark/mysql_interface.py
+++ benchmark/mysql_interface.py
@@ -3,6 +3,9 @@ from ssh_query import SSH_query
 from experiment import Experiment
 from pprint import pprint
 import time
+from functools import reduce
+import pandas as pd
+import numpy as np
 
 
 class SQL_Interface:
@@ -23,39 +26,81 @@ class SQL_Interface:
 
     # consider using other return type then list
 
-    def get_most_recent_experiment(self, args:str = '*',flag:bool = True) -> list:
+    def get_most_recent_experiment(self, args:str = '*',flag:bool = False) -> list:
         query = 'SELECT {0} from Experiment where id=(select max(id) from Experiment);'.format(args)
-        return self.tunnel.retrive_query(query,flag)
+        res = self.tunnel.retrive_query(query)
+        if(flag):
+            res = np.array(res)
+        return res
     
-    def get_all_experiments(self,args:str = '*', flag:bool=True):
+    def get_all_experiments(self,args:str = '*', flag:bool=False):
         query = 'SELECT {0} from Experiment;'.format(args)
-        return self.tunnel.retrive_query(query,flag)
+        res = self.tunnel.retrive_query(query)
+        if(flag):
+            res = np.array(res)
+        return res
     
-    def get_most_recent_invocations(self,args:str = '*', flag:bool = True):  
+    def get_most_recent_invocations(self,args:str = '*', flag:bool = False):  
         query = 'select {0} from Invocation where exp_id=(SELECT uuid from Experiment where id=(select max(id) from Experiment));'.format(args)
-        return self.tunnel.retrive_query(query,flag)
+        res = self.tunnel.retrive_query(query)
+        if(flag):
+            res = np.array(res)
+        return res
     
-    def get_most_recent_errors(self,args:str = '*', flag:bool = True):  
+    def get_most_recent_errors(self,args:str = '*', flag:bool = False):  
         query = 'select {0} from Error where exp_id=(SELECT uuid from Experiment where id=(select max(id) from Experiment));'.format(args)
-        return self.tunnel.retrive_query(query,flag)
+        res = self.tunnel.retrive_query(query)
+        if(flag):
+            res = np.array(res)
+        return res
     
-    def get_explicit_number_experiments(self,args:str= '*', number:int=1, flag:bool= True, order:bool= True):
+    def get_explicit_number_experiments(self,args:str= '*', number:int=1, flag:bool= False, order:bool= True):
         key_word = 'desc' if order else 'asc'
         query = 'SELECT {0} from Experiment order by id {1} limit {2};'.format(args,key_word,number)
-        return self.tunnel.retrive_query(query,flag)
-    
-    def get_explicit_number_invocations(self,args:str= '*', number:int=1, flag:bool= True, order:bool= True):
+        res = self.tunnel.retrive_query(query)
+        if(flag):
+            res = np.array(res)
+        return res
+
+    def get_explicit_number_invocations(self,args:str= '*', number:int=1, flag:bool= False, order:bool= True):
         key_word = 'desc' if order else 'asc'
         query = 'SELECT {0} from Invocation order by execution_start {1} limit {2};'.format(args,key_word,number)
-        return self.tunnel.retrive_query(query,flag)
-    
-    def get_explicit_number_errors(self,args:str= '*', number:int=1, flag:bool= True, order:bool= True):
+        res = self.tunnel.retrive_query(query)
+        if(flag):
+            res = np.array(res)
+        return res
+
+    def get_explicit_number_errors(self,args:str= '*', number:int=1, flag:bool= False, order:bool= True):
         key_word = 'desc' if order else 'asc'
         query = 'SELECT {0} from Error order by execution_start {1} limit {2};'.format(args,key_word,number)
-        return self.tunnel.retrive_query(query,flag)
+        res = self.tunnel.retrive_query(query)
+        if(flag):
+            res = np.array(res)
+        return res
+
     
-    def cpu_efficiency_lats_experiment(self,falg:bool = True):
-        calc:values = self.get_most_recent_invocations()
+    # note that this is not very usefull if invocations are nested
+    def cpu_efficiency_last_experiment(self):
+        cpu_efficiencies = self.cpu_efficiency_per_invocation()
+        return reduce(lambda x,y: x+y,cpu_efficiencies) / float(len(cpu_efficiencies))
+    
+    # note that this is not very usefull if invocations are nested
+    def cpu_efficiency_per_invocation(self):
+        # below link for formula
+        # https://www.ibm.com/support/knowledgecenter/en/SSVMSD_9.1.4/RTM_faq/rtm_calculating_cpu_efficiency.html
+        calc_values = np.array(self.get_most_recent_invocations('process_time,execution_total,function_cores'))
+        return [float(x[0])/float(x[1]*x[2]) for x in calc_values]
+    
+    def cpu_efficiency_for_throughput_per_invocation(self,exp_uuid:str=None):
+        exp_id = f"""'{exp_uuid}'""" if exp_uuid != None else '(SELECT uuid from Experiment where id=(select max(id) from Experiment))'
+        query = """SELECT throughput_process_time,throughput_running_time,function_cores,throughput_time FROM Invocation 
+                   WHERE throughput != 0 AND exp_id={0};""".format(exp_id)
+        data = self.tunnel.retrive_query(query)
+        return [(float(x[0])/float(x[1]*x[2]),x[3]) for x in data]
+
+    def cpu_efficiency_for_throughput_experiment(self,exp_uuid:str=None):
+        data = self.cpu_efficiency_for_throughput_per_invocation(exp_uuid)
+        return tuple(i for i in map(lambda x: x / float(len(data)), reduce(lambda x,y: (x[0]+y[0],x[1]+y[1]),data)))
     
     
     def get_all_from_Experiment(self,flag:bool=True):
