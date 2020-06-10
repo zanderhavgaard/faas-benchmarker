0ef1143d511951881423f919b58c2f6c43273847
zanderhavgaard <zander@havgaard.dk>
2020-06-10 19:48:35 +0200

    fixed status prints in the openfaas eks teardown script [ci skip]

--- eks_openfaas_orchestration/teardown_openfaas_eks_fargate.sh
+++ eks_openfaas_orchestration/teardown_openfaas_eks_fargate.sh
@@ -2,24 +2,24 @@
 
 set -e
 
-[ -z "$1" ] && echo "Please specify experiment name as first argument." && exit
+[ -z "$1" ] && errmsg "Please specify experiment name as first argument." && exit
 experiment_name=$1
 cluster_name="$experiment_name-openfaas-cluster"
 region="eu-west-1"
 
-echo -e "\nDestroying OpenFaas on AWS EKS/Fargate ..."
-echo -e "Experiment name: $experiment_name"
-echo -e "EKS cluster name: $cluster_name"
-echo -e "AWS region: $region\n"
+pmsg "\nDestroying OpenFaas on AWS EKS/Fargate ..."
+pmsg "Experiment name: $experiment_name"
+pmsg "EKS cluster name: $cluster_name"
+pmsg "AWS region: $region\n"
 
-echo -e "\nRemoving deployed OpenFaas functions ...\n"
+pmsg "\nRemoving deployed OpenFaas functions ...\n"
 
 faas-cli remove -f $fbrd/cloud_functions/openfaas/faas_benchmarker_functions.yml
 
-echo -e "\nDestroying EKS cluster ...\n"
+pmsg "\nDestroying EKS cluster ...\n"
 
 eksctl delete cluster \
     --name "$cluster_name" \
     --region "$region"
 
-echo -e "\nDone destroying OpenFaas infrastructure.\n"
+smsg "\nDone destroying OpenFaas infrastructure.\n"
