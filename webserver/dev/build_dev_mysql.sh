740afdafa9197251a4995f927cfa220dd9d1b36c
zanderhavgaard <zander@havgaard.dk>
2020-06-21 20:14:55 +0200

    made experiment data more readable on the webui, added some dev utils

---
+++ webserver/dev/build_dev_mysql.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t faasbenchmarker/exp_mysql:webui_dev .
--- webserver/fb_web_ui.py
+++ webserver/fb_web_ui.py
@@ -4,13 +4,14 @@ from flask import (
     Flask,
     render_template
 )
+import time
 
 app = Flask(__name__)
 db = DB_interface()
 
 
 @app.route('/')
-def hello_world():
+def index():
 
     # create the ascii banner
     banner = Figlet(width=120).renderText('faas-benchmarker')
@@ -21,11 +22,30 @@ def hello_world():
 
     experiment_keys = list(experiments[0].keys())
 
+    experiments_human_readable = make_experiment_data_more_readable(experiments)
+
     page = render_template('index.html',
                            banner=banner,
                            experiment_table_header=experiment_table_header,
                            experiment_keys=experiment_keys,
-                           experiments=experiments
+                           experiments=experiments_human_readable
                            )
 
     return page
+
+
+def make_experiment_data_more_readable(experiments: dict):
+    if experiments == None:
+        return None
+
+    for exp in experiments:
+        exp['dev_mode'] = 'true' if exp['dev_mode'] == 1 else 'false'
+        exp['start_time'] = human_readable_time(exp['start_time'])
+        exp['end_time'] = human_readable_time(exp['end_time'])
+        exp['total_time'] = human_readable_time(exp['total_time'])
+
+    return experiments
+
+
+def human_readable_time(seconds: float):
+    return time.strftime("%H:%M:%S", time.gmtime(seconds))
