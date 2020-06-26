4010a6edda1affd8d63c6354894006e80ead77c2
zanderhavgaard <zander@havgaard.dk>
2020-06-26 14:07:19 +0200

    added script for logging experiment lifecycle

--- orchestration/experiment_status_updater.sh
+++ orchestration/experiment_status_updater.sh
@@ -8,20 +8,23 @@ experiment_meta_identifier=$3
 function_provider=$4
 client_provider=$5
 
-docker_cmd="docker run --rm --network host mysql:5.7 mysql -uroot -pfaas -h127.0.0.1 -P3306 Benchmarks -e"
+ssh_command="ubuntu@$TF_VAR_db_server_static_ip"
 
 case "$command" in
     insert)
         pmsg "Inserting new ExperimentStatus row ..."
         experiment_status="running"
-        ssh_command="ubuntu@68.183.243.126"
         ssh $ssh_command " docker run --rm --network host mysql:5.7 mysql -uroot -pfaas -h127.0.0.1 -P3306 Benchmarks -e \" insert into ExperimentStatus (name, experiment_meta_identifier, function_provider, client_provider, status) values ('$experiment_name', '$experiment_meta_identifier', '$function_provider', '$client_provider', '$experiment_status') ;  \" "
         ;;
-    update_finished)
+    update_completed)
         pmsg "Updating ExperimentStatus row to completed ..."
+        experiment_status="completed"
+        ssh $ssh_command " docker run --rm --network host mysql:5.7 mysql -uroot -pfaas -h127.0.0.1 -P3306 Benchmarks -e \" update ExperimentStatus set status='$experiment_status' where name='$experiment_name' and experiment_meta_identifier='$experiment_meta_identifier' and function_provider='$function_provider' and client_provider='$client_provider' ;  \" "
         ;;
     update_failed)
         pmsg "Updaeting ExperimentStatus row to failed ..."
+        experiment_status="failed"
+        ssh $ssh_command " docker run --rm --network host mysql:5.7 mysql -uroot -pfaas -h127.0.0.1 -P3306 Benchmarks -e \" update ExperimentStatus set status='$experiment_status' where name='$experiment_name' and experiment_meta_identifier='$experiment_meta_identifier' and function_provider='$function_provider' and client_provider='$client_provider' ;  \" "
         ;;
     *)
         errmsg "Invalid command, valid commands are: insert, update_finished, update_failed"
