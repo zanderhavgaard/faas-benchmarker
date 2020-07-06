9a65e908d8eedbf0e005e099b3afcf4d5b6f2131
zanderhavgaard <zander@havgaard.dk>
2020-07-06 22:36:12 +0200

    fixed dragon deploy script ... [ci skip]

--- dev_tools/dragon_server/deploy_functions.sh
+++ dev_tools/dragon_server/deploy_functions.sh
@@ -3,6 +3,6 @@
 functions=$(ls openfaas_deploy_yaml )
 
 for yaml in $functions ; do
-   faas-cli remove -f "openfaas_deploy_yaml/$yaml" \
+   faas-cli deploy -f "openfaas_deploy_yaml/$yaml" \
       --label "com.openfaas.scale.zero=true"
 done
