295fd72c9d59f14b7ab4c8a6b3afc0318098d19f
zanderhavgaard <zander@havgaard.dk>
2020-07-31 16:17:33 +0200

    improved azure functions orchestrator, to verify that functions are indeed correctly deployed

--- orchestration/azure_functions_orchestrator.sh
+++ orchestration/azure_functions_orchestrator.sh
@@ -16,7 +16,16 @@ function bootstrap {
     bash init.sh "$experiment_name"
 
     pmsg "Creating cloud functions ..."
-    terraform apply -auto-approve
+    tf_deployed="false"
+    until $tf_deployed ; do
+        terraform apply -auto-approve \
+            && tf_deployed="true" \
+            && echo \
+            && pmsg "Successfully deployed azure functions using terraform ..."
+    done
+
+    pmsg "Outputting variables to $experiment_name-azure_functions.env ..."
+    terraform output > "$experiment_cloud_function_env"
 
     pmsg "Fixing broken terraform azure function code deployment ..."
 
@@ -27,26 +36,35 @@ function bootstrap {
         cd "$experiment_context/azure_functions/function_code/$fcd"
 
         # we retry the function app publish, as it might fail...
-        retries=10
+        retries=20
         counter=0
         deployed="false"
+        delay=60
         until $deployed ; do
             (( counter++ ))
             if [ $counter = $retries ] ; then
                 errmsg "Maximum deployment retries reached, aborting ..."
                 exit
             fi
-            pmsg "Trying functionapp deployment, attempt # $counter..."
-            func azure functionapp publish $fcd \
-                && deployed="true" \
-                && smsg "Successfully deployed function $fcd"
+            pmsg "Trying functionapp deployment, attempt # $counter / $retries ..."
+            func azure functionapp publish $fcd
+
+            # verify that the function was actually deployed correctly ...........
+            cd "$experiment_context/azure_functions/"
+            pmsg "attempting to invoke function ..."
+            curl -f -d '{}' "$(terraform output ${fcd}_function_app_url)/api/$fcd?code=$(terraform output ${fcd}_function_key)" \
+                && deployed="true"
+            cd "$experiment_context/azure_functions/function_code/$fcd"
+
+            if ! $deployed ; then
+                pmsg "Deployment failed, sleeping for $delay seconds, and will try again ..."
+                sleep $delay
+            else
+                smsg "Successfully deployed function $fcd"
+            fi
+
         done
     done
-
-    cd "$experiment_context/azure_functions"
-
-    pmsg "Outputting variables to $experiment_name-azure_functions.env ..."
-    terraform output > "$experiment_cloud_function_env"
 
     smsg "Done creating cloud functions."
 }
